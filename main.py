27d0186881db90768a825c64901706756e058cfe
joyboy-7 <a.narayana2003@gmail.com>
2024-11-20 17:13:34 +0530

    Added the table to display data

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ dummy_data: List[List[float]] = []
 active_clients: List[WebSocket] = []
 
 # Template directory for HTML responses
-templates = Jinja2Templates(directory=".")
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 async def get(request: Request):
@@ -20,6 +20,25 @@ async def get(request: Request):
     Homepage showing real-time sensor data visualization
     """
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+
+@app.get("/data")
+async def get_data():
+    """
+    Endpoint to view the dummy data as an HTML table.
+    """
+    # Create an HTML table from dummy_data
+    table_html = "<table border='1'><tr><th>Sensor_1</th><th>Sensor_2</th><th>Sensor_3</th><th>Sensor_4</th><th>Sensor_5</th><th>Sensor_6</th><th>Sensor_7</th><th>sensor_8</th><th>Sensor_9</th><th>Sensor_10</th><th>Sensor_11</th><th>Sensor_12</th><th>Sensor_13</th><th>Sensor_14</th><th>Sensor_15</th><th>Sensor_16</th></tr>"
+    
+    for row in dummy_data:
+        table_html += "<tr>" + "".join([f"<td>{col}</td>" for col in row]) + "</tr>"
+    
+    table_html += "</table>"
+    
+    return HTMLResponse(content=table_html, status_code=200)
+
+
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
